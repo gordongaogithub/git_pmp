472efb681ddeddddaf47f8a36c6fab432ad5cbd3
高超 <gordon.gao@leantek.cn>
2018-11-30 09:32:49 +0800

    init

---
+++ git_pmp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace git_pmp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("this is master");
+        }
+    }
+}
